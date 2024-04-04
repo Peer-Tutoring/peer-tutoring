c8efe1c2084a2c8c5ab9f32c6717ef871331055e
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-04-05 02:00:48 +0300

    setup paths aliases

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Title from "@components/Title";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="text-4xl text-white">Hello There</h1>
+      <Title />
     </main>
   );
 }
---
+++ frontend/src/components/Title.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Title = () => {
+  return <h1 className="text-4xl text-white">Hello There</h1>;
+};
+
+export default Title;
