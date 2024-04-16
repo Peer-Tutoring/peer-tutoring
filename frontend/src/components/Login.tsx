56244c29bce2964d9580241b5366e2350058a8a6
Shaar.03 <ashaar409@gmail.com>
2024-04-16 06:59:49 +0300

    make login and signup functional & remove remember me

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,19 +1,23 @@
 "use client";
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Login = () => {
   const [identifier, setIdentifier] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [remember, setRemember] = React.useState(false);
   const [message, setMessage] = React.useState("");
-
-  React.useEffect(() => {
-    // Retrieve identifier from local storage when component mounts
-    const storedIdentifier = localStorage.getItem("identifier");
-    if (storedIdentifier) {
-      setIdentifier(storedIdentifier);
-    }
-  }, []);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -26,19 +30,17 @@ const Login = () => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ identifier, password, remember }),
+          body: JSON.stringify({ identifier, password }),
         },
       );
 
       const data = await response.json();
       setMessage(data.message);
-
-      if (remember && data.success) {
-        // Save identifier to local storage when "Remember Me" is checked
-        localStorage.setItem("identifier", identifier);
-      } else if (!remember && data.success) {
-        // Remove identifier from local storage if it exists
-        localStorage.removeItem("identifier");
+      if (data.success) {
+        setIdentifier("");
+        setPassword("");
+      } else {
+        setPassword("");
       }
     } catch (error) {
       console.error("Error:", error);
@@ -47,39 +49,91 @@ const Login = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="flex flex-col">
-        <label htmlFor="identifier">Email or Username</label>
-        <input
-          type="text"
-          id="login-identifier"
-          value={identifier}
-          onChange={(e) => setIdentifier(e.target.value)}
-          required
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="login-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <div>
-          <input
-            type="checkbox"
-            id="remember"
-            name="remember"
-            checked={remember}
-            onChange={(e) => setRemember(e.target.checked)}
-            className="mr-2"
-          />
-          <label htmlFor="remember">Remember me</label>
-        </div>
-        <button type="submit" className="rounded-md bg-red-700 p-3">
-          Login
-        </button>
-      </form>
-      <p>{message}</p>
+      <Card className="mx-auto max-w-md">
+        <CardHeader>
+          <CardTitle className="text-center text-2xl">Welcome Back!</CardTitle>
+          <CardDescription>
+            Enter your Email or Username below to login back
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button variant="outline" className="w-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mr-2 h-6 w-6"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#FFC107"
+                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+              <path
+                fill="#FF3D00"
+                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+              ></path>
+              <path
+                fill="#4CAF50"
+                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+              ></path>
+              <path
+                fill="#1976D2"
+                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+            </svg>
+            Google
+          </Button>
+          <div className="relative mb-6 mt-6">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t"></span>
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                or continue with
+              </span>
+            </div>
+          </div>
+          <form onSubmit={handleSubmit} className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="identifier">Email/Username</Label>
+              <Input
+                type="text"
+                id="login-identifier"
+                value={identifier}
+                onChange={(e) => setIdentifier(e.target.value)}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="#"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input
+                type="password"
+                id="login-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <p className="text-red-500">{message}</p>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
