56db8ee5aec45e0a578e79c561450730a6943188
Shaar.03 <ashaar409@gmail.com>
2024-04-21 01:32:40 +0300

    ununsed import

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Link, redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function RootPage() {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
