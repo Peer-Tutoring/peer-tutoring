634d7e8318b8d1ea9105f325089f64e311964910
Shaar.03 <ashaar409@gmail.com>
2024-04-21 20:45:39 +0300

    add tab panel in registration

--- frontend/src/routes/auth/register/page.tsx
+++ frontend/src/routes/auth/register/page.tsx
@@ -1,189 +1,27 @@
-import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import { Link } from "react-router-dom";
-
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-
-import GoogleLogin from "@/components/auth/GoogleLogin";
+import StudentRegister from "@/components/StudentRegister";
+import TutorRegister from "@/components/TutorRegister";
 
 const RegisterPage = () => {
   if (localStorage.getItem("isAuthenticated")) {
     window.location.href = "/";
   }
 
-  const [message, setMessage] = React.useState("");
-  const formSchema = z.object({
-    name: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
-    password: z
-      .string()
-      .min(8, {
-        message: "Password must be at least 8 characters.",
-      })
-      .regex(new RegExp(".*[A-Z].*"), {
-        message: "Password must contain at least one uppercase letter.",
-      })
-      .regex(new RegExp(".*[0-9].*"), {
-        message: "Password must contain at least one number.",
-      })
-      .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"), {
-        message: "Password must contain at least one special character.",
-      })
-      .trim(),
-    email: z
-      .string()
-      .min(1, { message: "This field has to be filled." })
-      .email("This is not a valid email."),
-  });
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      password: "",
-      email: "",
-    },
-  });
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    try {
-      const response = await fetch(
-        "http://localhost/peer-tutoring/backend/signup.php",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(values),
-        },
-      );
-      const data = await response.json();
-      setMessage(data.message);
-      console.log(data.success);
-      if (data.success) {
-        window.location.href = "/auth/login";
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-
   return (
     <div className="mb-4 flex min-h-screen items-center justify-center">
-      <Card className="mx-auto max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-xxl text-center">
-            Create An Account!
-          </CardTitle>
-          <CardDescription>
-            Enter your information to create an account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <GoogleLogin />
-          <div className="relative mb-6 mt-6">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t"></span>
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                or continue with
-              </span>
-            </div>
-          </div>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input {...field} type="text" id="signup-name" required />
-                    </FormControl>
-                    <FormDescription />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        type="email"
-                        id="signup-email"
-                        placeholder="peer@example.com"
-                        required
-                      />
-                    </FormControl>
-                    <FormDescription />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        type="password"
-                        id="signup-password"
-                        required
-                      />
-                    </FormControl>
-                    <FormDescription />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit" className="w-full">
-                Create an account
-              </Button>
-            </form>
-          </Form>
-          <div className="mt-4 text-center text-sm">
-            Already have an account?{" "}
-            <Link to="/auth/login" className="underline">
-              Sign in
-            </Link>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <p>{message}</p>
-        </CardFooter>
-      </Card>
+      <Tabs defaultValue="student" className="m-4 w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="student">Student</TabsTrigger>
+          <TabsTrigger value="tutor">Tutor</TabsTrigger>
+        </TabsList>
+        <TabsContent value="student">
+          <StudentRegister />
+        </TabsContent>
+        <TabsContent value="tutor">
+          <TutorRegister />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 };
