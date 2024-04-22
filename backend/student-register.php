3ed91bd5001eba6e4a52a53e5d2a8eaffdb4fc69
Shaar.03 <ashaar409@gmail.com>
2024-04-22 06:43:12 +0300

    new

--- backend/student-register.php
+++ backend/student-register.php
@@ -14,34 +14,35 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $data = json_decode($inputData, true);
 
   // Check if required fields are set
-  if (!isset($data['name']) || !isset($data['email']) || !isset($data['password'])) {
+  if (!isset($data['firstName']) || !isset($data['lastName']) || !isset($data['email']) || !isset($data['password'])) {
     $response = array('error' => true, 'message' => 'Required fields are missing');
   } else {
     // Access individual fields
-    $name = mysqli_real_escape_string($conn, $data['name']);
+    $firstName = mysqli_real_escape_string($conn, $data['firstName']);
+    $lastName = mysqli_real_escape_string($conn, $data['lastName']);
     $email = mysqli_real_escape_string($conn, $data['email']);
     $password = mysqli_real_escape_string($conn, $data['password']);
 
-    // Check if name or email is already used
-    $checkQuery = "SELECT COUNT(*) AS count FROM user WHERE name = '$name' OR email = '$email'";
+    // Check if email is already used
+    $checkQuery = "SELECT COUNT(*) AS count FROM user WHERE email = '$email'";
     $checkResult = $conn->query($checkQuery);
 
     if ($checkResult && $checkResult->fetch_assoc()['count'] > 0) {
-      $response = array('error' => true, 'message' => 'Name or Email is already in use', 'success' => false);
+      $response = array('error' => true, 'message' => 'Email is already in use', 'success' => false);
     } else {
       // Hash the password
       $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
       // Insert data into the database
-      $insertQuery = "INSERT INTO user (name, email, password, date_created) VALUES ('$name', '$email', '$hashedPassword', '" . date('Y-m-d H:i:s') . "')";
+      $insertQuery = "INSERT INTO user (first_name, last_name, email, password, date_created) VALUES ('$firstName', '$lastName', '$email', '$hashedPassword', NOW())";
       $insertResult = $conn->query($insertQuery);
 
       if ($insertResult) {
         // Data inserted successfully
-        $response = array('error' => false, 'message' => 'Account created successfully', 'success' => true);
+        $response = array('message' => 'Account created successfully', 'success' => true);
       } else {
         // Failed to insert data
-        $response = array('error' => true, 'message' => 'Failed to insert data: ' . $conn->error, 'success' => false);
+        $response = array('message' => 'Failed to insert data: ' . $conn->error, 'success' => false);
       }
     }
   }
