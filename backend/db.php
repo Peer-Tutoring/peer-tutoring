23a91698e1316494121d1908e1e8248db06c5e4e
Shaar.03 <ashaar409@gmail.com>
2024-04-26 11:11:37 +0300

    fix dotenv

--- backend/db.php
+++ backend/db.php
@@ -1,9 +1,16 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "mydb";
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv as Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/');
+$dotenv->load();
+
+$db_host = $_ENV['HOSTNAME'];
+$db_user = $_ENV['DB_USER'];
+$db_password = $_ENV['DB_PASSWORD'];
+$db_name = $_ENV['DB_NAME'];
 $db_port = $_ENV['SQL_PORT'];
 
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name, $db_port);
