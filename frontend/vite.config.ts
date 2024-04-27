6291bca48be06b9ad504413b5d84f68d3636099e
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-04-27 13:41:06 +0300

    setup Unfonts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,24 @@
 import path from "path";
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react-swc";
 
+import Unfonts from "unplugin-fonts/vite";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unfonts({
+      google: {
+        families: [
+          {
+            name: "Rubik",
+            styles: "wght@300;400;500;600;700;800;900",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
